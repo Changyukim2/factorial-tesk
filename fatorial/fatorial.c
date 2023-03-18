5fe6a1a6215f88d36265bba02e3af37e15d0d04f
찬규김 찬규김 <kcg2994@naver.com>
2023-03-18 12:35:33 +0900

    초기 커밋.

---
+++ fatorial/fatorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+double factorial_rec(int n) {
+	if (n <= 1)return 1;
+	else return(n * factorial_rec(n - 1));
+
+}
+double factorial_iter(int n) {
+	int i = 1;
+	double result = 1;
+	for (i = 1; i <= n; i++)
+		result = result * i;
+	return(result);
+
+}
+
+int main(void) {
+	double result_rec = factorial_rec(20);
+	double result_iter = factorial_iter(20);
+	printf(" 20!의 값:%lf\n ", result_rec);
+	printf("20!의 값:%lf\n", result_iter);
+	system("pause");
+}
